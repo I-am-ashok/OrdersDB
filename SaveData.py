9bfb99d3a80a11090a2887736058dd2923aa536d
I-am-ashok <mashok4102@gmail.com>
2024-01-09 19:27:45 +0530

    updated SaveData.py

--- SaveData.py
+++ SaveData.py
@@ -1,4 +1,5 @@
 from Trasformations import *
+from SparkSession import *
 def savedata(df):
     df.write.saveAsTable("ordersdb.Orders",format="parquet",mode="overwrite")
-    return spark.sql("select count(*) as reccount from ordersdb.Orders")
+    return spark().sql("select count(*) as reccount from ordersdb.Orders")
