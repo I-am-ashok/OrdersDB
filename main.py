645057171af27e6101dcdeca22abc84730d04cfe
I-am-ashok <mashok4102@gmail.com>
2024-01-09 17:07:14 +0530

    updated main.py file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from Dataload import *
 from Trasformations import *
+from SaveData import *
 if __name__=='__main__':
     ordersdf=Dataload('ordjsonfile')
     orderItemsdf=Dataload('orditjsonfile')
     Transformationsdf(ordersdf,orderItemsdf).show()
+    savedata()
     #orderItemsdf.printSchema()
